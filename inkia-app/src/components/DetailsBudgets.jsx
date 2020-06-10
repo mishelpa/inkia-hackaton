e8373c498a9a41e9cfef5fbd2dd84c6f0195afcc
ayauri29 <albayauri.29@gmail.com>
2020-06-10 01:41:16 -0500

    Change states with buttons

--- inkia-app/src/components/DetailsBudgets.jsx
+++ inkia-app/src/components/DetailsBudgets.jsx
@@ -1,47 +1,60 @@
 import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import firebase from '../services/firebase';
-import MaterialTable from 'material-table'
+import { Card, Button } from 'react-bootstrap';
+import { Functions } from '../services/Functions';
 
 const DetailsBudgets = () => {
 	const { id } = useParams();
 	console.log(id)
 	const [budget, setBudget] = useState([])
 
-	
-	
-	useEffect(() => {
-		// firebase.firestore()
-		// 	.collection('budgets').doc(id).get()
-		// 	.then(doc => {
-		// 		setBudget(doc.data())
-		// 	});
+	const updateState = () => {
+		Functions.updateData('budgets', id, {estado: 'pendiente de aprobacion'})
+	}
 
-		firebase.firestore().collection('budgets').onSnapshot((querySnapshot) => {
-			const array = [];
-			querySnapshot.forEach((doc) => {
-				array.push({ id: doc.id, ...doc.data() });
+	useEffect(() => {
+		firebase.firestore()
+			.collection('budgets').doc(id).get()
+			.then(doc => {
+				setBudget(doc.data())
 			});
-			setBudget(array);
-		})
 	}, [])
 
 
-		return (
+	return (
 		<div style={{ maxWidth: '100%' }}>
-			<MaterialTable
-				columns={[
-					{ title: 'Adı', field: 'provider' },
-					{ title: 'Soyadı', field: 'concept' },
-					{ title: 'Doğum Yılı', field: 'subject'},
-					{ title: 'Doğum Yeri', field: 'company'}
-				]}
-				data={budget}
-				title="Demo Title"
-			/>
-		</div>
+			<h1>{budget.provider}</h1>
+			<Card style={{ width: '50rem' }}>
+				<Card.Header className="d-flex justify-content-between">
+					<div>Asunto: {budget.subject}</div>
+					<div>Responsable: {budget.corporative}</div>
+				</Card.Header>
+				<Card.Body>
+					<div className="d-flex justify-content-between">
+						<div className="d-flex flex-column">
+							<div>Empresa:</div>
+							<div>{budget.company}</div>
+						</div>
+						<div className="d-flex flex-column">
+							<div>Proveedor:</div>
+							<div>{budget.provider}</div>
+						</div>
+						<div className="d-flex flex-column">
+							<div>Tipo de proveedor:</div>
+							<div>{budget.type_provider}</div>
+						</div>
+					</div>
+					<div>Concepto: {budget.concept}</div>
+					<div>Moneda: {budget.currency}</div>
+					<div>Monto: {budget.total}</div>
+					<div>Tipo de cobro: {budget.type_charge}</div>
+				</Card.Body>
+			</Card>
+			<Button onClick={updateState()}>Enviar a factura</Button>
+		</div >
 	)
-	
+
 
 
 }
