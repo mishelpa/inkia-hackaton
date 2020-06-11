9b88954f46d6734234497487c7ed8955ed9cf8be
cielolili <cielito.torres@hotmail.com>
2020-06-11 16:30:29 -0500

    añade pantalla de carga

--- inkia-app/src/components/Loading.jsx
+++ inkia-app/src/components/Loading.jsx
@@ -1,6 +1,12 @@
 import React from 'react';
+import '../css/Loading.css';
 
 const Loading = () => (
-    <div className="loader">Loading...</div>
-  );
-  export default Loading;
+  <div>
+  <div class="preloader">
+  </div>
+   <h3 className="text-loading">Cargando...</h3>
+  </div>
+
+);
+export default Loading;
