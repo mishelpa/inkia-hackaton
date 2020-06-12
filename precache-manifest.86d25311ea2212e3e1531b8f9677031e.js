136706dfe9fa1a32424a3371633436cb01204091
mishelpa <judith.paredesartica@gmail.com>
2020-06-12 12:43:14 -0500

    Updates

---
+++ precache-manifest.86d25311ea2212e3e1531b8f9677031e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f7286077f0710678a483850924b07e80",
+    "url": "/inkia-hackaton/index.html"
+  },
+  {
+    "revision": "9ece262a9d6a5080890f",
+    "url": "/inkia-hackaton/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "a4497c576aff85e27e8b",
+    "url": "/inkia-hackaton/static/css/main.01d4d2e7.chunk.css"
+  },
+  {
+    "revision": "9ece262a9d6a5080890f",
+    "url": "/inkia-hackaton/static/js/2.16ffa8da.chunk.js"
+  },
+  {
+    "revision": "35578a2c9f6a06b5a4092790c19d11a1",
+    "url": "/inkia-hackaton/static/js/2.16ffa8da.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a4497c576aff85e27e8b",
+    "url": "/inkia-hackaton/static/js/main.16eeb8df.chunk.js"
+  },
+  {
+    "revision": "e1a31f6161731384a1e3",
+    "url": "/inkia-hackaton/static/js/runtime-main.bc599c4f.js"
+  },
+  {
+    "revision": "28e991d9b2e42c25357880df5eb965b3",
+    "url": "/inkia-hackaton/static/media/Logo.28e991d9.svg"
+  }
+]);
